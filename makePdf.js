cc8f7e96dc6b662e96ef24240c3d32de5c79e6b1
jeongmyeonghui <jeongmyeonghui@jeongmyeonghuis-MacBook-Pro.local>
2023-04-19 22:48:50 +0900

    zip

--- makePdf.js
+++ makePdf.js
@@ -1,8 +1,9 @@
 const AWS = require('aws-sdk')
 const s3= new AWS.S3();
 const fileBucketName = 'sample--bucket'
-
-
+const archiver = require('archiver');
+const { Readable } = require('stream');
+const { promisify } = require('util');
 const htmlToPdf = async (params) => {
     const puppeteer = require("puppeteer");
     try {
@@ -22,19 +23,43 @@ const htmlToPdf = async (params) => {
         throw e
     }
 }
+const zipBuffer = async (pdfBuffers, names) => {
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    const buffers = [];
 
+    // 'data' 이벤트 핸들러
+    archive.on('data', buffer => buffers.push(buffer));
+
+    // 'end' 이벤트 핸들러
+    const end = promisify(archive.on).bind(archive)('end');
+
+    // PDF 파일 버퍼를 압축 파일에 추가
+    for (let i = 0; i < pdfBuffers.length; i++) {
+        const pdfBuffer = pdfBuffers[i];
+        archive.append(pdfBuffer, { name: `${encodeURIComponent(names[i])}.pdf` });
+    }
+
+    // 압축 파일 생성
+    archive.finalize();
+
+    await end;
+
+    // 버퍼를 합쳐서 반환
+    return Buffer.concat(buffers);
+};
 const nf = async ()=>{
     const params = {
         htmlString: '<html><body><h1>Hello, world!</h1></body></html>'
-
     };
     const pdfBuffer = await htmlToPdf(params)
-
+    const pdfBuffers = [pdfBuffer, pdfBuffer]
+    const names = ['1.pdf', '2.pdf']
+    const buffer = await zipBuffer(pdfBuffers, names);
      await s3.putObject({
         Bucket: 'sample--bucket',
-        Key: 'f1.pdf',
-        Body: pdfBuffer, ContentType: 'application/pdf',
-        ContentDisposition: `attachment;filename="certificate.pdf"`,
+        Key: 'files.zip',
+        Body: buffer,
+        ContentDisposition: `attachment;filename="files.zip"`,
         ACL: 'private',
     }).promise()
 }
